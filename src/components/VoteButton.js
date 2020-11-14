a45f273d6219ac2488c0de94288ab3ac7e3be992
Asfian99 <mk.asfian@gmail.com>
2020-11-14 14:08:28 +0700

    disable vote button after user vote

--- src/components/VoteButton.js
+++ src/components/VoteButton.js
@@ -1,11 +1,45 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { IconButton, Text, VStack } from "@chakra-ui/core";
 import { FiArrowDown, FiArrowUp } from "react-icons/fi";
 import db from "../lib/firebase";
 
 const VoteButton = ({ post }) => {
+  const [isVoting, setIsVoting] = useState(false);
+  const [votedPost, setVotedPost] = useState([]);
+
+  useEffect(() => {
+    // Fetch the previously voted items from localStorage
+    const votesFromLocalStorage = localStorage.getItem("votes") || [];
+    let previousVotes = [];
+
+    try {
+      // Parse the value of the item from localStorage. If the value of the
+      // items isn't an array, then JS will throw an error.
+      previousVotes = JSON.parse(votesFromLocalStorage);
+    } catch (err) {
+      console.log(err);
+    }
+
+    setVotedPost(previousVotes);
+  }, []);
+
+  const handleDisablingOfVoting = (postId) => {
+    // This function is responsible for disabling the voting button after a
+    // user has voted. Fetch the previously voted items from localStorage. See
+    // https://stackoverflow.com/a/52607524/1928724 on why we need "JSON.parse"
+    // and update the item on localStorage.
+    const previousVotes = votedPost;
+    previousVotes.push(postId);
+
+    setVotedPost(previousVotes);
+
+    // Update the voted items from localStorage. See https://stackoverflow.com/a/52607524/1928724 on why we need "JSON.stringify" and update the item on localStorage.
+    localStorage.setItem("votes", JSON.stringify(votedPost));
+  };
+
   const handleClick = async (type) => {
     // asign in temporary variabel
+    setIsVoting(true);
     let upVotesCount = post.upVotesCount;
     let downVotesCount = post.downVotesCount;
     const date = new Date();
@@ -25,6 +59,18 @@ const VoteButton = ({ post }) => {
       createdAt: post.createdAt,
       updatedAt: date.toString(),
     });
+
+    // Disable the voting button once the voting is successful.
+    handleDisablingOfVoting(post.id);
+    setIsVoting(true);
+  };
+
+  const checkVoted = () => {
+    if (votedPost.indexOf(post.id) > -1) {
+      return true;
+    } else {
+      return false;
+    }
   };
 
   return (
@@ -37,6 +83,8 @@ const VoteButton = ({ post }) => {
           aria-label="Upvote"
           icon={<FiArrowUp />}
           onClick={() => handleClick("upvote")}
+          isLoading={isVoting}
+          isDisabled={checkVoted()}
         />
         <Text bg="gray.100" rounded="md" w="100%" p={1}>
           {post.upVotesCount}
@@ -50,6 +98,8 @@ const VoteButton = ({ post }) => {
           aria-label="Downvote"
           icon={<FiArrowDown />}
           onClick={() => handleClick("downvote")}
+          isLoading={isVoting}
+          isDisabled={checkVoted()}
         />
         <Text bg="gray.100" rounded="md" w="100%" p={1}>
           {post.downVotesCount}
